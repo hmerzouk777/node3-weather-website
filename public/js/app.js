e3dde2fef1a38edb410a09d744b876cbd05a0dea
Hammou Merzouk <hme@digital.security>
2020-05-03 20:19:49 +0200

    Remove unnecessary condole.log all

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
 document.addEventListener('DOMContentLoaded', () => {
     const weatherForm = document.querySelector('form')
     const search = document.querySelector('input')
