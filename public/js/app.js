493a4e27f82b8ab68015cdec921b364c9b25de56
Hammou Merzouk <hme@digital.security>
2020-05-03 20:15:54 +0200

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side javascript file is loaded!')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const weatherForm = document.querySelector('form')
+    const search = document.querySelector('input')
+
+    const message1 = document.querySelector('#message-1')
+    const message2 = document.querySelector('#message-2')
+
+    weatherForm.addEventListener('submit', (event) =>{
+        event.preventDefault()
+
+        const location = search.value
+        const url = 'http://localhost:3000/weather?address=' + location
+
+        fetch(url).then((response) => {
+            response.json().then((data) => {
+                if(data.error){
+                    message1.textContent = data.error
+                    message2.textContent = ''
+                }else{
+                    message1.textContent = data.location
+                    message2.textContent = data.forecast
+                }
+            })
+        })
+    })
+})
