046b35aab129e5244dd1e070fa2980c4431fce33
Hammou Merzouk <hme@digital.security>
2020-05-03 22:40:04 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault()
 
         const location = search.value
-        const url = 'http://localhost:3000/weather?address=' + location
+        const url = '/weather?address=' + location
 
         fetch(url).then((response) => {
             response.json().then((data) => {
